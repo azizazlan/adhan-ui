59a81215e8e90dafe3511f450eaefce7eead30a2
Azlan <azlan@thuleen.io>
2024-10-10 17:35:31 +0800

    Fixed next prayer

--- src/components/devmode/Devmode.tsx
+++ src/components/devmode/Devmode.tsx
@@ -4,7 +4,9 @@ import styles from './Devmode.module.scss';
 
 interface DevmodeProps {
   toggleTestScreenIqamah: () => void;
+  toggleTestScreenPrayers: () => void;
   toggleTestSubuh: () => void;
+  toggleTestSyuruk: () => void;
   toggleRefetch: () => void;
   lastApiTimestamp: number;
 }
@@ -14,13 +16,21 @@ const Devmode = (props: DevmodeProps) => {
   return (
     <div class={styles.container}>
       <button class={styles.testButton} onClick={() => props.toggleTestScreenIqamah()}>Iqamah Screen</button>
+      <button class={styles.testButton} onClick={() => props.toggleTestScreenPrayers()}>Prayers Screen</button>
       <button class={styles.testButton} onClick={() => props.toggleTestSubuh()}>Test Subuh</button>
+      <button class={styles.testButton} onClick={() => props.toggleTestSyuruk()}>Test Syuruk</button>
       <button class={styles.testButton} onClick={() => props.toggleRefetch()}>Refetch</button>
       <div class={styles.paramsContainer}>
-        <h5>Production params</h5>
+        <h4>Production params</h4>
         <div>VITE_ADHAN_LEAD_MINS: {import.meta.env.VITE_ADHAN_LEAD_MINS}</div>
-        <h5>Dev params</h5>
+        <div>VITE_IQAMAH_INTERVAL_MS: {import.meta.env.VITE_IQAMAH_INTERVAL_MS}
+          ({parseFloat(import.meta.env.VITE_IQAMAH_INTERVAL_MS) / (1000 * 60)} mins)</div>
+        <hr />
+        <h4>Dev params</h4>
         <div>VITE_ADHAN_LEAD_MINS_TEST: {import.meta.env.VITE_ADHAN_LEAD_MINS_TEST}</div>
+        <div>VITE_IQAMAH_INTERVAL_MS_TEST: {import.meta.env.VITE_IQAMAH_INTERVAL_MS_TEST}
+          ({parseFloat(import.meta.env.VITE_IQAMAH_INTERVAL_MS_TEST) / 1000} secs)
+        </div>
         <hr />
         <div>Last fetch api timestamp: {lastApiTimestamp()}</div>
         <div>Formatted: {format(lastApiTimestamp() * 1000, 'dd/MM/yyyy')}</div>
--- src/components/prayer/PrayerBox.tsx
+++ src/components/prayer/PrayerBox.tsx
@@ -6,8 +6,20 @@ import { PrayerMode } from '../../types/prayer';
 const PrayerBox = (props: PrayerBoxProps) => {
   const prayer = createMemo(() => props.prayer);
 
+  if (prayer().mode === PrayerMode.IMMEDIATE_NEXT) {
+    return (
+      <div class={styles.nextPrayerContainer}>
+        <div class={styles.name}>
+          {prayer().name}
+        </div>
+        <div class={styles.time}>
+          {prayer().time}
+        </div>
+      </div>
+    );
+  }
   return (
-    <div class={`${styles.container} ${prayer().mode === PrayerMode.IMMEDIATE_NEXT ? styles.active : ''}`}>
+    <div class={`${styles.container} ${prayer().mode === PrayerMode.ACTIVE ? styles.active : ''}`}>
       <div class={styles.name}>
         {prayer().name}
       </div>
